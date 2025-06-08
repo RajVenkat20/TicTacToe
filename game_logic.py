927efaab1741665bade55dc6ac3c8829598c9a6d
Raj Venkat Reddy Mavuram <mrajvenkatreddy@gmail.com>
2025-06-08 17:48:49 -0500

    Added comments in game_logic.py

--- game_logic.py
+++ game_logic.py
@@ -8,6 +8,7 @@ class TicTacToe:
         self.current_player = 'X'
         self.winner = None
 
+    # Function to make a move on the board and shift turns among the players
     def make_move(self, position):
         if self.board[position] != BLANK_SQUARE or self.winner:
             return False
@@ -17,6 +18,7 @@ class TicTacToe:
             self.current_player = 'O' if self.current_player == 'X' else 'X'
         return True
 
+    # Function that checks if the current board state results in the user winning or not
     def check_winner(self):
         size = self.board_size
         b = self.board
@@ -28,7 +30,7 @@ class TicTacToe:
             if [b[j*size + i] for j in range(size)].count(b[i]) == size and b[i] != BLANK_SQUARE:
                 return b[i]
 
-        # Diagonals
+        # Check Diagonals
         if [b[i*(size+1)] for i in range(size)].count(b[0]) == size and b[0] != BLANK_SQUARE:
             return b[0]
         if [b[(i+1)*(size-1)] for i in range(size)].count(b[size-1]) == size and b[size-1] != BLANK_SQUARE:
@@ -36,9 +38,11 @@ class TicTacToe:
 
         return None
 
+    # Function that checks if the board state results in a draw
     def is_draw(self):
         return BLANK_SQUARE not in self.board and not self.winner
 
+    # Function to reset the current board state to the initial state
     def reset(self):
         self.board = [BLANK_SQUARE] * (self.board_size ** 2)
         self.current_player = 'X'

12ab022a42fe95da9d8df9d3d3174bdcb93149c0
Raj Venkat Reddy Mavuram <mrajvenkatreddy@gmail.com>
2025-06-08 17:45:51 -0500

    Added comments in main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+# Starting point for the improvised Tic-Tac-Toe game.
+# Run this file to open the Tic-Tac-Toe gui
 from gui import TicTacToeGUI
 
 if __name__ == "__main__":
+    # By default, the board size is initialized to 3
     app = TicTacToeGUI(board_size=3)
     app.run()
